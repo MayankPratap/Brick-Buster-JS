e080bc7d7eb715a6b142cd2dd0c39a173d854481
Mayank Pratap <mayank7860@gmail.com>
2016-06-30 23:57:28 +0530

    Now ball bounces back properly from all 4 walls

--- js/game.js
+++ js/game.js
@@ -4,13 +4,15 @@ var ctx=canvas.getContext("2d");
 var x=canvas.width/2;
 var y=canvas.height-30;
 
+var ballRadius=10;
+
 var dx=2;
 var dy=-2;
 
 function drawBall(){
 
   ctx.beginPath();
-  ctx.arc(x,y,10,0,Math.PI*2);
+  ctx.arc(x,y,ballRadius,0,Math.PI*2);
   ctx.fillStyle="#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -19,8 +21,17 @@ function drawBall(){
 function draw(){
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    x+=dx;
-    y+=dy;
+    drawBall();
+
+    if(y+dy<ballRadius || y+dy>canvas.height-ballRadius)
+      dy=-dy;
+    else
+      y+=dy;
+
+    if(x+dx<ballRadius || x+dx>canvas.width-ballRadius)
+       dx=-dx;
+    else
+       x+=dx;
 
 }
 
