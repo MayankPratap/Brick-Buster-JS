af4a3b8bead74dd7a590ca3bd1e34185490fba2c
Mayank Pratap <mayank7860@gmail.com>
2016-07-01 08:10:06 +0530

    Added paddle moving logic

--- js/game.js
+++ js/game.js
@@ -3,7 +3,11 @@ var ctx=canvas.getContext("2d");
 
 var x=canvas.width/2;
 var y=canvas.height-30;
-
+var paddleHeight=10;
+var paddleWidth=75;
+var paddleX=(canvas.width-paddleWidth)/2;
+var rightPressed=false;  // Whether right control button is pressed
+var leftPressed=false;  // Whether left control button is pressed
 var ballRadius=10;
 
 var dx=2;
@@ -18,10 +22,21 @@ function drawBall(){
   ctx.closePath();
 }
 
+function drawPaddle(){
+
+  ctx.beginPath();
+  ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+  ctx.fillStyle="#0095DD";
+  ctx.fill();
+  ctx.closePath();
+
+}
+
 function draw(){
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawBall();
+    drawPaddle();
 
     if(y+dy<ballRadius || y+dy>canvas.height-ballRadius)
       dy=-dy;
@@ -33,6 +48,34 @@ function draw(){
     else
        x+=dx;
 
+    if(rightPressed && paddleX<canvas.width-paddleWidth)
+       paddleX+=7;
+    else if(leftPressed && paddleX>0)
+       paddleX-=7;
+
 }
+
+document.addEventListener("keydown",keyDownHandler,false);
+document.addEventListener("keyup",keyUpHandler,false);
+
+function keyDownHandler(e){
+
+  if(e.keyCode==39)
+    rightPressed=true;
+  else if(e.keyCode==37)
+    leftPressed=true;
+
+
+}
+
+function keyUpHandler(e){
+
+   if(e.keyCode==39)
+    rightPressed=false;
+   if(e.keyCode==37)
+     leftPressed=false;
+
+}
+
 
 setInterval(draw,10);  // Run this every 10 millisecond
