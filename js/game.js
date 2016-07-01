7509d823faa576eb5500552e8ecdb8b548ec7147
Mayank Pratap <mayank7860@gmail.com>
2016-07-01 08:43:34 +0530

    Now paddle and ball are interacting

--- js/game.js
+++ js/game.js
@@ -38,8 +38,19 @@ function draw(){
     drawBall();
     drawPaddle();
 
-    if(y+dy<ballRadius || y+dy>canvas.height-ballRadius)
+    if(y+dy<ballRadius)
       dy=-dy;
+    else if(y+dy>canvas.height-ballRadius){
+
+       if(x>paddleX && x<paddleX+paddleWidth)
+          dy=-dy;
+       else{
+          alert("Game Over");
+          document.location.reload();
+       }
+
+
+    }
     else
       y+=dy;
 
